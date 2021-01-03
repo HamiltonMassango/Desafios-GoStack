cd1e2df0da6dd1bc44119a2711397e9263420a93
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-03 21:16:15 +0100

    add function router

---
+++ BACK_AND/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import router from './routes';
+
+import './database';
+
+class App {
+    constructor(){
+        this.server = express();
+    }
+    middlewares(){
+        
+    }
+    routes(){
+        this.server.use(router);
+    }
+}
