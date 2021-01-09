23dca575a1e1daf843a85c1707676c7d3187e5d6
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-01-09 06:13:12 +0100

    add auth in routes

--- BACK_AND/src/routes.js
+++ BACK_AND/src/routes.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 
 import sessionControllers from './app/controllers/SessionControllers';
 import studentControllers from './app/controllers/StudentsControllers';
+import auth from './app/middlewares/auth';
 
 const router = new Router();
 
 router.post('/sessions', sessionControllers.store);
-
-router.post('/student', studentControllers.store);
+router.use(auth);
+router.post('/students', studentControllers.store);
 
 export default router;
