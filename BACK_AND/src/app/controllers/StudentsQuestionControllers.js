527fb3c81bee974c6a06a251075e9c3ee936e50e
HamiltonMassango <hamiltonsilva551@gmail.com>
2021-03-16 22:05:56 +0100

    cadrastrar questions

--- BACK_AND/src/app/controllers/StudentsQuestionControllers.js
+++ BACK_AND/src/app/controllers/StudentsQuestionControllers.js
@@ -2,8 +2,14 @@ import Help_Orders from '../models/Help_orders';
 
 class StudentsQuestions {
   async store(req, res, next) {
-    const question = await Help_Orders.create(req.body);
-    return res.json(question);
+    const { question } = req.body;
+    const { id: student_id } = req.body;
+
+    const questions = await Help_Orders.create({
+      student_id,
+      question,
+    });
+    return res.json(questions);
   }
 }
 
